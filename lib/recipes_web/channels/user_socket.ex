d61b691bfb40c594d17e11ec9607dfb996dae16a
Anna Trifonova <anna.a.trifonova@gmail.com>
2018-10-13 19:22:47 +0200

    Setup Heroku

--- lib/recipes_web/channels/user_socket.ex
+++ lib/recipes_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule RecipesWeb.UserSocket do
   # channel "room:*", RecipesWeb.RoomChannel
 
   ## Transports
-  transport(:websocket, Phoenix.Transports.WebSocket)
+  transport(:websocket, Phoenix.Transports.WebSocket, timeout: 45_000)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
