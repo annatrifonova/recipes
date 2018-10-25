199c27ca311c0efc34a2f384b5d56e63c9fef13b
Anna Trifonova <anna.a.trifonova@gmail.com>
2018-10-25 15:30:31 +0200

    Add update and delete actions for ingridient

--- test/recipes_web/controllers/ingridient_controller_test.exs
+++ test/recipes_web/controllers/ingridient_controller_test.exs
@@ -30,8 +30,60 @@ defmodule RecipesWeb.IngridientControllerTest do
     end
   end
 
+  describe "edit ingridient" do
+    setup [:create_ingridient]
+
+    test "renders form for editing chosen ingridient", %{conn: conn, recipe: recipe, ingridient: ingridient} do
+      conn = get(conn, recipe_ingridient_path(conn, :edit, recipe, ingridient))
+      assert html_response(conn, 200) =~ "Edit Ingridient"
+    end 
+  end
+
+  describe "update ingridient" do
+    setup [:create_ingridient]
+
+    test "redirects when data is valid", %{conn: conn, recipe: recipe, ingridient: ingridient} do
+      %{name: name, quantity: quantity} = params = params_for(:ingridient)
+
+      conn = put(conn, recipe_ingridient_path(conn, :update, recipe, ingridient), ingridient: params)
+      assert redirected_to(conn) == recipe_path(conn, :show, recipe)
+
+      response = 
+        conn
+        |> get(recipe_path(conn, :show, recipe))
+        |> html_response(200)
+
+      assert response =~ name
+      assert response =~ quantity
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, recipe: recipe, ingridient: ingridient} do
+      params = params_for(:ingridient, name: "")
+      conn = put(conn, recipe_ingridient_path(conn, :update, recipe, ingridient), ingridient: params)
+      assert html_response(conn, 200) =~ "Edit Ingridient"
+    end
+  end
+
+  describe "delete ingridient" do
+    setup [:create_ingridient]
+
+    test "deletes chosen ingridient", %{conn: conn, recipe: recipe, ingridient: ingridient} do
+      conn = delete(conn, recipe_ingridient_path(conn, :delete, recipe, ingridient))
+      assert redirected_to(conn) == recipe_path(conn, :show, recipe)
+
+      assert_error_sent(404, fn ->
+        get(conn, recipe_ingridient_path(conn, :edit, recipe, ingridient))
+      end)
+    end
+  end
+
   defp create_recipe(_) do
     recipe = insert(:recipe)
     {:ok, recipe: recipe}
+  end
+
+  defp create_ingridient(%{recipe: recipe}) do
+    ingridient = insert(:ingridient, recipe: recipe)
+    {:ok, ingridient: ingridient}
   end
 end
