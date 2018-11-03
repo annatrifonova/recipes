3ca4abb08afc5611c70a43f8890d0e78a5cb7b0a
Anna Trifonova <anna.a.trifonova@gmail.com>
2018-11-03 22:59:59 +0100

    Split product and ingridient

--- lib/recipes/cookbook/product.ex
+++ lib/recipes/cookbook/product.ex
@@ -7,7 +7,14 @@ defmodule Recipes.Cookbook.Product do
     field(:name, :string)
 
     has_many(:ingridients, Ingridient, on_delete: :nothing)
-    
+
     timestamps()
+  end
+
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name)
   end
 end
--- test/recipes/cookbook/ingridient_test.exs
+++ test/recipes/cookbook/ingridient_test.exs
@@ -4,20 +4,18 @@ defmodule Recipes.Cookbook.IngridientTest do
   import Recipes.Factory
 
   alias Recipes.Cookbook
-  alias Recipes.Cookbook.Ingridient
+  alias Recipes.Cookbook.{Ingridient, Product}
 
   describe "list_ingridients/1" do
     test "returns all ingridients for the specified recipe" do
       ingridient = insert(:ingridient)
-      %{id: ingridient_id, recipe: recipe} = ingridient
+      %{id: ingridient_id, product_id: product_id, recipe: recipe} = ingridient
       %{id: recipe_id} = recipe
-
-      # Same as:
-      # %{id: ingridient_id, recipe: %{id: recipe_id} = recipe} = insert(:ingridient)
 
       assert [
                %Ingridient{
                  id: ^ingridient_id,
+                 product_id: ^product_id,
                  recipe_id: ^recipe_id
                }
              ] = Cookbook.list_ingridients(recipe)
@@ -26,52 +24,126 @@ defmodule Recipes.Cookbook.IngridientTest do
 
   describe "get_ingridient!/1" do
     test "returns the ingridient with given id" do
-      %{id: id, recipe_id: recipe_id, name: name, quantity: quantity} = insert(:ingridient)
+      %{id: id, recipe_id: recipe_id, product_id: product_id, quantity: quantity} =
+        insert(:ingridient)
 
-      assert %Ingridient{id: ^id, recipe_id: ^recipe_id, name: ^name, quantity: ^quantity} =
-               Cookbook.get_ingridient!(id)
+      assert %Ingridient{
+               id: ^id,
+               recipe_id: ^recipe_id,
+               product_id: ^product_id,
+               quantity: ^quantity
+             } = Cookbook.get_ingridient!(id)
     end
   end
 
   describe "create_ingridient/1" do
-    test "creates a new ingridient with valid attributes" do
+    test "creates a new ingridient using the existing product" do
       %{id: recipe_id} = insert(:recipe)
-      %{name: name, quantity: quantity} = params = params_for(:ingridient, recipe_id: recipe_id)
+      %{id: product_id, name: name} = insert(:product)
+      %{quantity: quantity} = params_for(:ingridient)
+
+      assert {:ok, %Ingridient{} = ingridient} =
+               Cookbook.create_ingridient(%{
+                 name: name,
+                 quantity: quantity,
+                 recipe_id: recipe_id
+               })
+
+      assert %{
+               quantity: ^quantity,
+               recipe_id: ^recipe_id,
+               product_id: ^product_id
+             } = ingridient
+    end
+
+    test "creates a new ingridient and new product" do
+      %{id: recipe_id} = insert(:recipe)
+      %{name: name} = params_for(:product)
+      %{quantity: quantity} = params_for(:ingridient)
+
+      params = %{
+        name: name,
+        quantity: quantity,
+        recipe_id: recipe_id
+      }
 
       assert {:ok, %Ingridient{} = ingridient} = Cookbook.create_ingridient(params)
 
       assert %{
-               name: ^name,
                quantity: ^quantity,
-               recipe_id: ^recipe_id
+               recipe_id: ^recipe_id,
+               product_id: product_id
              } = ingridient
+
+      assert %Product{id: ^product_id} = Cookbook.find_product(%{name: name})
     end
 
     test "returns error changeset with invalid attributes" do
       %{id: recipe_id} = insert(:recipe)
-      params = params_for(:ingridient, recipe_id: recipe_id, name: "")
+
+      params = %{
+        name: "",
+        quantity: "",
+        recipe_id: recipe_id
+      }
+
       assert {:error, %Ecto.Changeset{}} = Cookbook.create_ingridient(params)
     end
   end
 
   describe "update_ingridient/2" do
-    test "update the ingridient with valid attributes" do
+    test "update the ingridient using existing product" do
       %{id: ingridient_id, recipe_id: recipe_id} = ingridient = insert(:ingridient)
-      %{name: name, quantity: quantity} = params = params_for(:ingridient)
+      %{id: product_id, name: name} = insert(:product)
+      %{quantity: quantity} = params_for(:ingridient)
+
+      params = %{
+        name: name,
+        quantity: quantity,
+        recipe_id: recipe_id
+      }
 
       assert {:ok, updated_ingridient} = Cookbook.update_ingridient(ingridient, params)
 
       assert %Ingridient{
                id: ^ingridient_id,
                recipe_id: ^recipe_id,
-               name: ^name,
+               product_id: ^product_id,
                quantity: ^quantity
              } = updated_ingridient
     end
 
+    test "updates the ingridient and creates a new product" do
+      %{id: ingridient_id, recipe_id: recipe_id} = ingridient = insert(:ingridient)
+      %{name: name} = params_for(:product)
+      %{quantity: quantity} = params_for(:ingridient)
+
+      params = %{
+        name: name,
+        quantity: quantity,
+        recipe_id: recipe_id
+      }
+
+      assert {:ok, updated_ingridient} = Cookbook.update_ingridient(ingridient, params)
+
+      assert %Ingridient{
+               id: ^ingridient_id,
+               recipe_id: ^recipe_id,
+               product_id: product_id,
+               quantity: ^quantity
+             } = updated_ingridient
+
+      assert %Product{id: ^product_id} = Cookbook.find_product(%{name: name})
+    end
+
     test "returns error changeset with invalid attributes" do
-      ingridient = insert(:ingridient)
-      params = params_for(:ingridient, name: "")
+      %{recipe_id: recipe_id} = ingridient = insert(:ingridient)
+
+      params = %{
+        name: "",
+        quantity: "",
+        recipe_id: recipe_id
+      }
 
       assert {:error, %Ecto.Changeset{}} = Cookbook.update_ingridient(ingridient, params)
     end
